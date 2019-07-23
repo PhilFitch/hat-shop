97479030d8da5f169515a12352860ce7f6ad4668
Phil Fitch <mostreluctantly@gmail.com>
2019-07-22 21:45:58 -0700

    added hats data and image files

---
+++ src/products.js
@@ -0,0 +1,93 @@
+
+const fedora = {
+    code: 'fedora',
+    name: 'Fedora',
+    image: 'assets/fedora.jpg',
+    description: 'Classic Noir',
+    category: 'felt',
+    price: 75,
+    cost: 40,
+};
+
+const trilby = {
+    code: 'trilby',
+    name: 'Trilby',
+    image: 'assets/trilby.png',
+    description: 'For the Man in the Grey Flannel Suit.',
+    category: 'tweed',
+    price: 70,
+    cost: 35,
+};
+
+const panama = {
+    code: 'panama',
+    name: 'Panama',
+    image: 'assets/panama.jpg',
+    description: 'Think: Carmen Sandiego',
+    category: 'straw',
+    price: 65,
+    cost: 25,
+};
+
+const bowler = {
+    code: 'bowler',
+    name: 'Bowler',
+    image: 'assets/bowler.jpg',
+    description: 'The Tramp',
+    category: 'felt',
+    price: 70,
+    cost: 40,
+};
+
+const newsboy = {
+    code: 'newsboy',
+    name: 'Newsboy',
+    image: 'assets/newsboy.jpg',
+    description: 'Extra! Extra!',
+    category: 'tweed',
+    price: 60,
+    cost: 30,
+};
+
+const flatCap = {
+    code: 'flatCap',
+    name: 'Flat Cap',
+    image: 'assets/flatCap.jpg',
+    description: 'Perfect for a drive in the county.',
+    category: 'tweed',
+    price: 70,
+    cost: 35,
+};
+
+const boater = {
+    code: 'boater',
+    name: 'Boater',
+    image: 'assets/boater.jpg',
+    description: 'Punting on the lake, anyone?',
+    category: 'straw',
+    price: 50,
+    cost: 20,
+};
+
+const porkPie = {
+    code: 'porkPie',
+    name: 'Pork Pie',
+    image: 'assets/porkPie.jpg',
+    description: 'A beebop prerequisite.',
+    category: 'felt',
+    price: 70,
+    cost: 30,
+};
+
+const hats = [
+    fedora,
+    trilby,
+    panama,
+    bowler,
+    newsboy,
+    flatCap,
+    boater,
+    porkPie,
+];
+
+export default hats;
