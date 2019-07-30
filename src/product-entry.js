e06f944a25a7a76c93b21ae881296f644acce880
Phil Fitch <mostreluctantly@gmail.com>
2019-07-29 18:46:49 -0700

    submit hooked up

---
+++ src/product-entry.js
@@ -0,0 +1,20 @@
+import store from './store.js';
+
+const form = getElementById('new-hat');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const newHat = {
+        code: formData.get('code'),
+        name: formData.get('name'),
+        image: formData.get('image'),
+        description: formData.get('description'),
+        category: formData.get('category'),
+        price: +formData.get('price'),
+        cost: +formData.get('cost');
+    };
+    store.addHat(newHat);
+    alert('Hat Added!');
+    form.reset();
+});
